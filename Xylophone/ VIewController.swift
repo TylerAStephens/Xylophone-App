f0c81ea56f8339c5602577f672f02ad8265862e6
TylerAStephens <tylerstephens16@yahoo.com>
2018-10-31 00:10:45 -0400

    added function to access proper audio files per tag

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -12,8 +12,13 @@ import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate{
     
-    var audioPlayer: AVAudioPlayer!
-    //Variable to call audio player
+    var audioPlayer : AVAudioPlayer!
+    // Variable to call audio player
+    var selectedSoundFileName : String = ""
+    // Declared here to make global variable to prevent any scope issues
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    // Array of sound files
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -21,10 +26,16 @@ class ViewController: UIViewController, AVAudioPlayerDelegate{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
-        
-      let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
+        selectedSoundFileName = soundArray[sender.tag - 1]
+        // sender.tag to pick out selected file from tag of button pressed
+        // - 1 used in order to pick from proper order in array
+        playSound()
+    }
+
+    
+    func playSound() {
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
         // Sound URL path
-        
         do{
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
             // Directory Path of sound file to initialize soundplayer with contents of soundURL
@@ -34,7 +45,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate{
             // print any error into console to debug
         }
         audioPlayer.play()
-  
     }
-
+    
+    
 }
